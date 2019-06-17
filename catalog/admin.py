feaca36ebb080638530de2e199ccf0e85b1f5a83
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-17 15:07:11 -0400

    Finish admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -19,10 +19,14 @@ admin.site.register(Author, AuthorAdmin)
 
 # Register the Admin classes for Book using the decorator
 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
 # Register the Admin classes for BookInstance using the decorator
 
