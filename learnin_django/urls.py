f656eed5b4ab417417cb2d35229bea6c07892956
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-17 13:40:12 -0400

    Define models

--- learnin_django/urls.py
+++ learnin_django/urls.py
@@ -13,6 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import RedirectView
+from django.urls import include
 from django.contrib import admin
 from django.urls import path
 
@@ -20,18 +24,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-from django.urls import include
-from django.urls import path
 urlpatterns += [
-    path('catalog/', include ('catalog.urls')),
+    path('catalog/', include('catalog.urls')),
 ]
 
-from django.views.generic import RedirectView
 urlpatterns += [
     path('', RedirectView.as_view(url='/catalog/', permanent=True))
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
