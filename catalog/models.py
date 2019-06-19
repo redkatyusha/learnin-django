982cb2d6ed7375aefd50e1441252ad11378c1300
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-19 09:40:23 -0400

    Add first half of authhentication stuff

--- catalog/models.py
+++ catalog/models.py
@@ -2,6 +2,10 @@ import uuid  # Required for unique book instances
 # Used to generate URLs by reversing the URL patterns
 from django.urls import reverse
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
+
+
 
 # Create your models here.
 
@@ -56,6 +60,8 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -78,6 +84,13 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
+
 
 
 class Author(models.Model):
